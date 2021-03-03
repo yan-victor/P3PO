a96c687439415c2eb60e1a0d060e9895623a5140
yan-victor <48385460+yan-victor@users.noreply.github.com>
2021-03-03 17:43:08 -0300

    Create Media1.cpp

---
+++ Aula1/Media1.cpp
@@ -0,0 +1,13 @@
+//URI - 1005
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main () {
+  double x,y;
+  cin>>x>>y;
+  cout.precision(5);
+  cout.setf(ios::fixed);
+  cout<<"MEDIA = "<<(3.5*x+7.5*y)/11<<"\n";
+}
