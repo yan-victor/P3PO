73a9a727d5fd14ab780f2f2b9dffdd5293deeba7
yan-victor <48385460+yan-victor@users.noreply.github.com>
2021-03-10 16:48:17 -0300

    Create GastoCombustivel.cpp

---
+++ Aula2/GastoCombustivel.cpp
@@ -0,0 +1,17 @@
+//URI 1017 - Gasto de Combustível
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main () {
+  double a,b,c;
+  cin>>a>>b>>c;
+  cout.precision(3);
+  cout.setf(ios::fixed);
+  double pi = 3.14159;
+  cout<<"TRIANGULO: "<<a*c/2<<"\n";
+  cout<<"CIRCULO: "<<pi*c*c<<"\n";
+  cout<<"TRAPEZIO: "<<(a+b)*c/2<<"\n";
+  cout<<"QUADRADO: "<<b*b<<"\n";
+  cout<<"RETANGULO: "<<a*b<<"\n";
+}
