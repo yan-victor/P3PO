c421b387977faecd4872ca8c59e3559ca58e8d23
yan-victor <48385460+yan-victor@users.noreply.github.com>
2021-03-10 17:12:19 -0300

    Create ParImparV2.cpp

---
+++ Aula2/ParImparV2.cpp
@@ -0,0 +1,19 @@
+//OBI PJ Fase 1 - 2016 - Jogo de Par ou Ímpar
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int p,alice,bob;
+	cin>>p>>alice>>bob;
+	if((p+alice+bob)%2==0) cout<<"0\n";
+	else cout<<"1\n";
+	/*if(p==0) {
+		if((alice+bob)%2==0) cout<<"0\n";
+		else cout<<"1\n";
+	} else {
+		if((alice+bob)%2==0) cout<<"1\n";
+		else cout<<"0\n";
+	}*/
+}
