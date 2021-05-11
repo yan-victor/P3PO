a99536671a3ef4995035fa8c1408eb2752e5d213
Yan Victor <yn.guimaraes@gmail.com>
2021-05-11 18:30:46 -0300

    fix spur rocks

--- Aula9/spurrocks.cpp
+++ Aula9/spurrocks.cpp
@@ -11,12 +11,7 @@ int pontos[N];
 
 bool comp(int a,int b) {
 	if(pontos[a]!=pontos[b]) return pontos[a]>pontos[b];
-	double aavg,bavg;
-	if(cestasrecebidas[a]>0) aavg = (double)cestas[a]/(double)cestasrecebidas[a];
-	else aavg = (double)cestas[a];
-	if(cestasrecebidas[b]>0) bavg = (double)cestas[b]/(double)cestasrecebidas[b];
-	else bavg = (double)cestas[b];
-	if(aavg!=bavg) return aavg>bavg;
+	if(cestas[a]*cestasrecebidas[b]!=cestas[b]*cestasrecebidas[a]) return cestasrecebidas[b]*cestas[a]>cestas[b]*cestasrecebidas[a];
 	if(cestas[a]!=cestas[b]) return cestas[a]>cestas[b];
 	return a<b;
 
@@ -25,9 +20,13 @@ bool comp(int a,int b) {
 int main() {
 	int n,a,b,c,d;
 	cin>>n;
-	int intstancia = 0;
+	int instancia = 0;
 	while(n!=0) {
-		intstancia++;
+		if(instancia) cout<<"\n";
+		instancia++;
+		memset(cestas,0,sizeof(cestas));
+		memset(cestasrecebidas,0,sizeof(cestasrecebidas));
+		memset(pontos,0,sizeof(pontos));
 		for(int i=0;i<(n*(n-1))/2;i++) {
 			cin>>a>>b>>c>>d;
 			cestas[a]+=b;
@@ -39,11 +38,13 @@ int main() {
 		}
 		for(int i=1;i<=n;i++) {
 			v[i]=i;
+			if(cestasrecebidas[i]==0) cestasrecebidas[i]=1;
 		}
 		sort(v+1,v+n+1,comp);
-		cout<<"Intstancia "<<intstancia<<"\n";
-		for(int i=1;i<=n;i++)
-			cout<<v[i]<<" ";
+		cout<<"Instancia "<<instancia<<"\n";
+		cout<<v[1];
+		for(int i=2;i<=n;i++)
+			cout<<" "<<v[i];
 		cout<<"\n";
 		cin>>n;
 	}
